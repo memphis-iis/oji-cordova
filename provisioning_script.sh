94c3ad4685fb107caddd7cab7952a9f38008ccf4
JRustyHaner <jrhaner@memphis.edu>
2021-12-02 14:28:12 -0600

    fixed vagrant box

--- provisioning_script.sh
+++ provisioning_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Make a symbolic link to the sync'ed directory for more "natural" work
-ln -s /vagrant "$HOME/cordova-testing"
+ln -s /vagrant "$HOME/oji"
 
 # We will need to be able to compile some binary packages for Meteor
 sudo apt-get update
@@ -12,15 +12,19 @@ sudo apt-get install -y make
 sudo apt-get install -y automake
 sudo apt-get install -y git
 sudo apt-get install -y unzip
-sudo apt-get install -y lxde
+
 
 ###############################################################################
 # Install MongoDB
 
-
+# Use MongoDB 4.2
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-sudo systemctl enable mongodsudo apt-get install xfce4
+
+sudo systemctl enable mongod
+
 sudo apt-get install dos2unix
 
 # Change mongo to listen on all addresses (which is fine since we're walled off)
@@ -52,6 +56,22 @@ unzip commandlinetools-linux-7583922_latest.zip
 rm commandlinetools-linux-7583922_latest.zip
 cd cmdline-tools
 mkdir tools
+
+sudo umount .meteor/local -f
+rm .meteor/local -rf
+mkdir -p .meteor/local
+
+sudo umount packages -f
+rm packages -rf
+mkdir -p packages
+
+mkdir -p "$HOME/.meteor/local"
+sudo mount --bind "$HOME/.meteor/local" .meteor/local
+
+mkdir -p "$HOME/.meteor/packages"
+sudo mount --bind "$HOME/.meteor/packages" packages
+
+
 mv ./* ~/android/cmdline-tools/tools/
 
 #write bashrc
@@ -60,7 +80,7 @@ echo 'export ANDROID_SDK_ROOT=$HOME/android' >> ~/.bashrc
 echo 'export PATH=$ANDROID_HOME/cmdline-tools/tools/bin/:$PATH' >> ~/.bashrc
 echo 'export PATH=$ANDROID_HOME/emulator/:$PATH'  >> ~/.bashrc
 echo 'export PATH=$ANDROID_HOME/platform-tools/:$PATH' >> ~/.bashrc
-echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> ~/.bashrc
+echo 'export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64' >> ~/.bashrc
 
 #export bash vars
 export ANDROID_HOME=$HOME/android
@@ -68,7 +88,7 @@ export ANDROID_SDK_ROOT=$HOME/android
 export PATH=$ANDROID_HOME/cmdline-tools/tools/bin/:$PATH
 export PATH=$ANDROID_HOME/emulator/:$PATH
 export PATH=$ANDROID_HOME/platform-tools/:$PATH
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 
 #install node and nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
@@ -85,6 +105,13 @@ meteor npm install --save babel-runtime --no-bin-links
 
 #Install gradle
 sudo apt-get install -y gradle
+
+#meteor kung-fu
+sudo umount .meteor/local -f
+rm .meteor/local -rf
+mkdir -p .meteor/local
+
+
 
 # Remove Ubuntu's landscape stuff and clear login messages
 sudo apt-get purge -y landscape-client landscape-common
