9b0076159a7a33a32e0e016330b25626c1e7b1b3
Rhodes White <rawhite2@memphis.edu>
2021-12-07 13:20:15 -0600

    created login template for a user

--- oji/client/views/login.js
+++ oji/client/views/login.js
@@ -1,13 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
-function loginUser(){
-    Meteor.loginWithPassword($('#usernameInput')[0].value, $('#passwordInput')[0].value);
-    //clears the input boxes on submit
-    $('#usernameInput')[0].value = '';
-    $('#passwordInput')[0].value = '';
-}
-
 Template.login.events({
-    'click #login-submit': loginUser()
+    'click #login-submit': function() {
+        event.preventDefault();
+        Meteor.loginWithPassword($('#usernameLogin')[0].value, $('#passwordLogin')[0].value);
+        //clears the Login boxes on submit
+        $('#usernameLogin')[0].value = '';
+        $('#passwordLogin')[0].value = '';
+    }
 })
--- oji/imports/ui/App.jsx
+++ oji/imports/ui/App.jsx
@@ -7,7 +7,7 @@ export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
     <Blaze template="login"/>
-    {/* <Hello/>
-    <Info/> */}
+    <Hello/>
+    <Info/>
   </div>
 );
