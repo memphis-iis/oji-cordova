1d9c4f623398cdd7250b239119d5cf5f99d90342
JRustyHaner <jrhaner@memphis.edu>
2021-12-07 12:10:38 -0600

    added react back in to iron router

--- oji/client/index.js
+++ oji/client/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { Template } from 'meteor/templating';
+import { App } from '../imports/ui/App.jsx';
+
+Template.DefaultLayout.onRendered(() => {
+    render(<App/>, document.getElementById('react-target'));
+  });
