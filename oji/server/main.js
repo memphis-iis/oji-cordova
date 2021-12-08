b96e01fb5b05139832849ff7ec042a73b42a23c7
Rhodes White <rawhite2@memphis.edu>
2021-12-08 09:49:41 -0600

    added roles

--- oji/server/main.js
+++ oji/server/main.js
@@ -1,15 +1,27 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base'
+import { Roles } from 'meteor/alanning:roles'; // https://github.com/Meteor-Community-Packages/meteor-roles
 
 const SEED_USERNAME = 'testUser';
 const SEED_PASSWORD = 'password';
+const SEED_ROLES = ['user', 'supervisor', 'admin']
 
 Meteor.startup(() => {
+    //create seed roles
+    for(let role of SEED_ROLES){
+        if(!Meteor.roles.findOne({ '_id' : role })._id){
+            Roles.createRole(role);
+        }
+    }
+
     //create seed user
     if (!Accounts.findUserByUsername(SEED_USERNAME)) {
         Accounts.createUser({
             username: SEED_USERNAME,
             password: SEED_PASSWORD,
         });
+        const uid = Accounts.findUserByUsername(SEED_USERNAME)._id;
+        Roles.addUsersToRoles(uid, 'supervisor')
+        console.log(SEED_USERNAME + ' is in role supervisor? ' + Roles.userIsInRole(uid, 'supervisor'))
     }
 });
