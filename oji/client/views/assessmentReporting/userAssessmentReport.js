c08a7331cffe218e57d37cf84b72b0996e728960
Rhodes White <rawhite2@memphis.edu>
2022-01-21 10:04:41 -0600

    updated userAssessmentReport

--- oji/client/views/assessmentReporting/userAssessmentReport.js
+++ oji/client/views/assessmentReporting/userAssessmentReport.js
@@ -1,9 +1,22 @@
 Template.userAssessmentReport.helpers({
     'assessment': function() {
-        Trials.findOne({'userId': Meteor.userId(), 'identifier': 'PICTS'}).subscaleTotals.MOLL;
-    },
+        let trial = Trials.findOne({'userId': Meteor.userId(), 'identifier': 'CCSM'});
+        let assessment = Assessments.findOne({"identifier": "CCSM"});
+        subscales = Object.keys(trial.subscaleTotals);
+        console.log(Trials.findOne({'userId': Meteor.userId(), 'identifier': 'CCSM'}))
+        let scales = [];
+        for(let i = 0; i < subscales.length; i++){
+            scales[i] = {
+                'subscaleName': assessment.subscaleTitles[subscales[i]],
+                'subscaleScore': trial.subscaleTotals[subscales[i]]
+            }
+        }
+        console.log(scales)
+        return scales;
+    }
 })
 
 Template.userAssessmentReport.onCreated(function() {
     Meteor.subscribe('usertrials');
+    Meteor.subscribe('assessments');
 });
