a181df210aee9d8bd0430ae0f105ad1610349af7
James Rusty Haner <jrhaner@memphis.edu>
2022-08-09 10:35:58 -0500

    adds json uploader for modules

--- oji/client/views/adminControlPanel.js
+++ oji/client/views/adminControlPanel.js
@@ -317,40 +317,40 @@ Template.adminControlPanel.events({
     },
     'change #fileInput'(e, template) {
         if (e.currentTarget.files && e.currentTarget.files[0]) {
-          // We upload only one file, in case
-          // multiple files were selected
-          const upload = Files.insert({
-            file: e.currentTarget.files[0],
-            chunkSize: 'dynamic'
-          }, false);
-    
-          upload.on('start', function () {
-            template.currentUpload.set(this);
-          });
-    
-          upload.on('end', function (error, fileObj) {
-            if (error) {
-              alert(`Error during upload: ${error}`);
-            } else {
-              alert(`File "${fileObj.name}" successfully uploaded`);
-              link = Files.link(fileObj);
-              fileName = fileObj.name;
-              type = fileObj.type;
-              Meteor.call('addFileToOrg',  link, fileName, type);
-              if(fileObj.ext == "json"){
-                Meteor.call('uploadModule',fileObj.path,Meteor.userId(),function(err,res){
-                    if(err){
-                        alert("package failed");
-                    } else {
-                        console.log(res);
-                    }
-                });
-            }
-            }
-            template.currentUpload.set(false);
-          });
-    
-          upload.start();
+            // We upload only one file, in case
+            // multiple files were selected
+            const upload = FileStore.insert({
+              file: e.currentTarget.files[0],
+              chunkSize: 'dynamic'
+            }, false);
+      
+            upload.on('start', function () {
+              template.currentUpload.set(this);
+            });
+      
+            upload.on('end', function (error, fileObj) {
+              if (error) {
+                alert(`Error during upload: ${error}`);
+              } else {
+                alert(`File "${fileObj.name}" successfully uploaded`);
+                link = FileStore.link(fileObj);
+                fileName = fileObj.name;
+                type = fileObj.type;
+                Meteor.call('addFileToOrg',  link, fileName, type);
+                if(fileObj.ext == "json"){
+                  Meteor.call('uploadModule',fileObj.path,Meteor.userId(),function(err,res){
+                      if(err){
+                          alert("package failed");
+                      } else {
+                          console.log(res);
+                      }
+                  });
+              }
+              }
+              template.currentUpload.set(false);
+            });
+      
+            upload.start();
         }
     },
     'click #delete-file': function (event){
